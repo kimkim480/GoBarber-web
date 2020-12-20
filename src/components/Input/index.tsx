f908b225b9519dce8fa4434c8cf20e863dd3c31f
kimkim480 <lordkim480@gmail.com>
2020-12-20 13:11:36 -0300

    validando cadastro

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,55 @@
+import React, {
+  InputHTMLAttributes,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
+
+import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsfocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  const handleInputFocus = useCallback(() => {
+    setIsfocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsfocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+  return (
+    <Container isFilled={isFilled} isFocused={isFocused}>
+      {Icon && <Icon size={20} />}
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default Input;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,63 @@
+import React, { useCallback } from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string()
+          .required('E-mail é obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mńimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: true,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="/">
+          <FiArrowLeft />
+          Voltar para o Logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
