ad5553c160bcfd924c2c19e40ec77e6601c32438
kimkim480 <lordkim480@gmail.com>
2020-12-28 16:05:15 -0300

    autenticaçao

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import GlobalStyle from './styles/global';
 
 import SignIn from './pages/SignIn/index';
 
+import { AuthProvider } from './hooks/AuthContext';
+
 // import SignUp from './pages/SignUp/index';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+
     <GlobalStyle />
   </>
 );
